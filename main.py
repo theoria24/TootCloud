55b19b640c3f178acce30976a136d70996322265
theoria <theoria23@gmail.com>
2018-04-05 04:16:20 +0900

    投稿するトゥートにメディアのURLを含めないように
    新しめのmastodonだとURLが挿入されなくなったので

--- main.py
+++ main.py
@@ -210,7 +210,6 @@ def toot():
         api_base_url = session['uri'])
     path = "./static/out/" + img + ".png"
     media_files = [mstdn.media_post(media, "image/png") for media in [path]]
-    text += " " + media_files[0]['text_url']
     status = mstdn.status_post(status=text, media_ids=media_files, visibility=vsbl)
     url = status['url']
     return render_template('toot.html', toot_url=url, status="logout", site_url=app.config['SITE_URL'])
